146728ccdbba99ec07fe114940d24702f7523c19
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2022-11-12 23:07:33 +0300

    Подключил eslint, настроил и заиспользовал ts

--- server.ts
+++ server.ts
@@ -3,13 +3,15 @@ const fastify = Fastify({
     logger: true
 })
 
-fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
+fastify.get('/', async () => {
+    return {
+        hello: 'world'
+    }
 })
 
 const start = async () => {
     try {
-        await fastify.listen({ port: 8080 })
+        await fastify.listen({port: 8080})
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
