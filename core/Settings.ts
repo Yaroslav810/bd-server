769320519600931c52278cbeb557394730a79f46
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2022-11-13 17:05:37 +0300

    Добавил prizma и сделал тестовые запросы

--- core/Settings.ts
+++ core/Settings.ts
@@ -3,27 +3,17 @@ import {Logger} from './Logger'
 
 class Settings {
     readonly APP_PORT: number
-    readonly DB_HOST: string
-    readonly DB_NAME: string
-    readonly DB_USER: string
-    readonly DB_PASSWORD: string
-    readonly DB_PORT: number
+    readonly DATABASE_URL: string
 
     constructor() {
         dotenv.config()
-        if (!process.env['APP_PORT'] || !process.env['DB_HOST']
-        || !process.env['DB_NAME'] || !process.env['DB_USER']
-        || !process.env['DB_PASSWORD'] || !process.env['DB_PORT']) {
+        if (!process.env['APP_PORT'] || !process.env['DATABASE_URL']) {
             Logger.error('You need to fill in the .env file')
             throw new Error('No data in .env')
         }
 
         this.APP_PORT = parseInt(process.env['APP_PORT'] as string)
-        this.DB_HOST = process.env['DB_HOST'] as string
-        this.DB_NAME = process.env['DB_NAME'] as string
-        this.DB_USER = process.env['DB_USER'] as string
-        this.DB_PASSWORD = process.env['DB_PASSWORD'] as string
-        this.DB_PORT = parseInt(process.env['DB_PORT'] as string)
+        this.DATABASE_URL = process.env['DB_HOST'] as string
     }
 }
 
--- server.ts
+++ server.ts
@@ -1,14 +1,28 @@
 import Fastify from 'fastify'
 import {settings} from "./core/Settings"
+import { PrismaClient } from '@prisma/client'
 
 const fastify = Fastify({
     logger: true
 })
+const prisma = new PrismaClient()
 
 fastify.get('/', async () => {
     return {
         hello: 'world'
     }
+})
+
+fastify.post('/create_user', async () => {
+    await prisma.user.create({
+        data: {
+            login: 'Yaroslav',
+            first_name: 'Yaroslav',
+            password: 'New password',
+            type: "user",
+        },
+    })
+    return null
 })
 
 const start = async () => {
